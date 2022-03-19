4acf0cc3378b11452066bddf764c3a67c221eca8
perian <dmperian@gmail.com>
2022-03-19 18:08:57 +0200

    Созданы компоненты страниц приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,26 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main/main';
+
+type Genre = {
+  href: string;
+  name: string;
+}
+
+type AppScreenProps = {
+  promoFilmTitle: string;
+  promoFilmGenre: string;
+  promoFilmYear: number;
+  catalogGenres: Genre[];
+}
+
+function App({promoFilmTitle, promoFilmGenre, promoFilmYear, catalogGenres}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      promoFilmTitle={promoFilmTitle}
+      promoFilmGenre={promoFilmGenre}
+      promoFilmYear={promoFilmYear}
+      catalogGenres={catalogGenres}
+    />
+  );
 }
 
 export default App;
