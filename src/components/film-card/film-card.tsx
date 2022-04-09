5a63ac34a9109a0edf711772fc16566c2381f9b6
perian <dmperian@gmail.com>
2022-04-09 21:32:20 +0300

    Добавил обработчик активного фильма

---
+++ src/components/film-card/film-card.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { Film } from '../../types/film';
+
+type FilmCardProps = {
+  film: Film;
+  setActiveFilm:(film: Film | null) => void;
+}
+
+function FilmCard(props: FilmCardProps): JSX.Element {
+  const { film, setActiveFilm } = props;
+
+  const onMouseOver = () => {
+    setActiveFilm(film);
+  };
+
+  const onMouseOut = () => {
+    setActiveFilm(null);
+  };
+
+  return (
+    <article className="small-film-card catalog__films-card" data-id={film.title} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+      <div className="small-film-card__image">
+        <img src={film.imgSrc} alt={film.title} width="280" height="175" />
+
+      </div>
+      <h3 className="small-film-card__title">
+        <Link className="small-film-card__link" to={`/films/${film.id}`}>{film.title}</Link>
+      </h3>
+    </article>
+  );
+}
+
+export default FilmCard;
